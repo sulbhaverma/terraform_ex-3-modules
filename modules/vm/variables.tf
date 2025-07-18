50c7f8ce719456994ec5468b9658bf225651cdc4
sulbhaverma <148425570+sulbhaverma@users.noreply.github.com>
2025-07-18 18:40:29 +0530

    Create variables.tf

---
+++ modules/vm/variables.tf
@@ -0,0 +1,7 @@
+variable "vm_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "subnet_id" {}
+variable "nsg_id" {}
+variable "admin_username" {}
+variable "admin_password" {}

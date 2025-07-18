5b79dda3d45c11311ff55b18e4e39427f86eb7f6
sulbhaverma <148425570+sulbhaverma@users.noreply.github.com>
2025-07-18 18:39:57 +0530

    Create main.tf

---
+++ modules/vm/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
